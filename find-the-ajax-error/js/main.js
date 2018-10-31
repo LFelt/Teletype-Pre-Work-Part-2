0e7d3f3d0acca4c520757df4194d671419d1960c
LFelt <felt.larissa@gmail.com>
2018-10-31 17:27:19 -0600

    Fix git push errors

--- find-the-ajax-error/js/main.js
+++ find-the-ajax-error/js/main.js
@@ -5,14 +5,15 @@ $('.btn').on('click', function() {
     dataType: 'jsonp',
      success: function(dataWeGotViaJsonp){
       // console.log(dataWeGotViaJsonp.results.length);
-      $('.text').text(''); 
+      $('.text').text('');
       var results = dataWeGotViaJsonp.results;
       for(var i=0; i<results.length; i++){
-        var city = results.city;
-        $('#citylist').append('<li>' + cities + '</li>');
+        var city = results[i].city;
+        $('#citylist').append('<li>' + city + '</li>');
       }
     }
   });
 });
 
 $(document).ready("DOMContentLoaded")
+   
