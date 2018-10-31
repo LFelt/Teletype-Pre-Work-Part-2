4954517e1abd1830c24315c56cb56219bc18ae1f
ShambreSW <shambre.senawright@techtonic.com>
2018-10-31 14:18:57 -0600

    Update main.js with important info.

--- find-the-ajax-error/js/main.js
+++ find-the-ajax-error/js/main.js
@@ -3,13 +3,13 @@ $('.btn').on('click', function() {
   $.ajax({
     url: 'https://api.meetup.com/2/cities',
     dataType: 'jsonp',
-    success: function(dataWeGotViaJsonp){
+     success: function(dataWeGotViaJsonp){
       // console.log(dataWeGotViaJsonp.results.length);
-      $('.text').text('');
+      $('.text').text(''); 
       var results = dataWeGotViaJsonp.results;
       for(var i=0; i<results.length; i++){
-        var city = results[i].city;
-        $('#citylist').append('<li>' + city + '</li>');
+        var city = results.city;
+        $('#citylist').append('<li>' + cities + '</li>');
       }
     }
   });
