b1af0c0388ebf9b70d7f27389371e1293493932d
ShambreSW <shambre.senawright@techtonic.com>
2018-10-31 14:10:44 -0600

    Update main.js
    Very important changes to main.js

--- js-list-display-challenge/js/main.js
+++ js-list-display-challenge/js/main.js
@@ -4,11 +4,3 @@ var cities = [
   "Black Hawk",
   "Aurora"
 ];
-
-for (var i = 0; i < cities.length; i++) {
-  var bullet = '<li>' + cities[i] + '</li>';
-  document.querySelector('ul.list-cities').append(bullet);
-};
-
-
-console.log(document.querySelector('ul.list-cities'));
